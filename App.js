440cf50db90ac2050172a4bbb9a10940efed1a0f
hasanbeker22 <hasanbeker22@gmail.com>
2023-02-03 01:07:56 +0100

    new try

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function App() {
+import SettingsScreen from './Settings';
+import MenuScreen from './Menu';
+import FotosScreen from './Fotos';
+import InfoScreen from './Info';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Settings"
+        activeColor="#063970"
+        inactiveColor="#1e81b0"
+        barStyle={{ backgroundColor: '#abdbe3' }}
+      >
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="tree" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Menu"
+          component={MenuScreen}
+          options={{
+            tabBarLabel: 'Menu',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="food" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Fotos"
+          component={FotosScreen}
+          options={{
+            tabBarLabel: 'Fotos',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="image" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Info"
+          component={InfoScreen}
+          options={{
+            tabBarLabel: 'Info',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="information" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <BottomTabNavigator />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const InfoScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>This is the Info Screen</Text>
+        </View>
+    );
+};
+
+export default InfoScreen;
